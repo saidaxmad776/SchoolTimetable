5aab90ce466146514187f514de6f9448b2580337
ASaidaxmad <saidaxmad7760746@gmail.com>
2022-09-04 15:05:19 +0500

    Create onboarding View Controller

---
+++ SchoolTimetable/Controllers/OnbordingVC/OnbordingVC.swift
@@ -0,0 +1,16 @@
+//
+//  OnbordingVC.swift
+//  SchoolTimetable
+//
+//  Created by Test on 04/09/22.
+//
+
+import UIKit
+
+class OnbordingVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
